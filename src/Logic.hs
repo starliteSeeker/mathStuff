4443f1434715110e9e19b6445c965d83b8998fbf
Albert Lee <lee3445@purdue.edu>
2023-07-10 20:40:17 +0800

    better parsing method

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,10 +1,12 @@
 module Logic (makeTruth) where
 
+import Control.Applicative ((<**>))
 import Data.List (nub)
 import Data.Maybe (fromJust)
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
 data Expr
   = EBool Bool
@@ -20,51 +22,59 @@ type Result = [([Bool], Bool)]
 
 type Lookup = [(Var, Bool)]
 
+--
 -- Parser
--- not the best use case for LL(1) parsers
+--
+
+-- E -> T E'
+-- E' -> "||" E | e
+-- T -> P T'
+-- T' -> "&&" T | e
+-- P -> '(' E ')' | '!' P | "true" | "false" | Variable
 type Parser = Parsec Void String
 
-expr :: Parser Expr
-expr = choice [EBool <$> tf, EVar <$> var, notp, try orp, andp]
+skipSpace :: Parser ()
+skipSpace = L.space space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme skipSpace
+
+parseE :: Parser Expr
+parseE = parseT <**> parseE'
+
+parseE' :: Parser (Expr -> Expr)
+parseE' = flip EOr <$> (string "||" >> skipSpace >> parseE) <|> return id
+
+parseT :: Parser Expr
+parseT = parseP <**> parseT'
+
+parseT' :: Parser (Expr -> Expr)
+parseT' = flip EAnd <$> (string "&&" >> skipSpace >> parseT) <|> return id
+
+parseP :: Parser Expr
+parseP = lexeme $ choice [between (lexeme $ char '(') (char ')') parseE, parseNot, EBool <$> tf, EVar <$> var]
+
+parseNot :: Parser Expr
+parseNot = ENot <$> (char '!' >> skipSpace >> parseP)
 
 tf :: Parser Bool
 tf = label "boolean" $ True <$ string' "true" <|> False <$ string' "false"
 
-orp :: Parser Expr
-orp =
-  between
-    (char '(')
-    (char ')')
-    ( do
-        e1 <- expr
-        string "||"
-        EOr e1 <$> expr
-    )
-
-andp :: Parser Expr
-andp =
-  between
-    (char '(')
-    (char ')')
-    ( do
-        e1 <- expr
-        string "&&"
-        EAnd e1 <$> expr
-    )
-
-notp :: Parser Expr
-notp = ENot <$> (char '!' >> between (char '(') (char ')') expr)
-
 var :: Parser Var
 var = label "variable" $ some alphaNumChar
 
-readExpr :: String -> Either (ParseErrorBundle String Void) Expr
-readExpr = parse expr "input"
+-- the main parsing function
+parseExpr :: String -> Either (ParseErrorBundle String Void) Expr
+parseExpr = parse (between skipSpace eof parseE) "input"
 
--- making truth table
+--
+-- Making truth table
+--
+
 truth :: [Var] -> Expr -> Result
 truth vs ex = map (\lk -> (map snd lk, eval ex lk)) $ enumerate vs
 
+-- Get a list of variables used in the expression
 getVars :: Expr -> [Var]
 getVars ex = nub $ getVars' ex
   where
@@ -74,9 +84,11 @@ getVars ex = nub $ getVars' ex
     getVars' (ENot a) = getVars a
     getVars' _ = []
 
+-- Generate all possible combinations of true/false
 enumerate :: [Var] -> [Lookup]
 enumerate = foldr (\v -> ((:) <$> [(v, False), (v, True)] <*>)) [[]]
 
+-- Evaluate a expression with given assignment of variables
 eval :: Expr -> Lookup -> Bool
 eval (EBool b) _ = b
 eval (EOr a b) lk = eval a lk || eval b lk
@@ -84,22 +96,29 @@ eval (EAnd a b) lk = eval a lk && eval b lk
 eval (ENot a) lk = not $ eval a lk
 eval (EVar v) lk = fromJust $ lookup v lk
 
+-- Pretty(?) print the result
 printResult :: [Var] -> Result -> IO ()
 printResult vs res = print vs >> mapM_ print res
 
 -- | Create truth table with boolean expression
--- /many/ parenthesis are needed
+-- Operator precedence: NOT > AND > OR
 --
--- >>> makeTruth "(a||(!(a)&&b))"
--- ["a","b"]
--- ([False, False], False)
--- ([False, True], True)
--- ([True, False], True)
--- ([True, True], True)
+-- >>> makeTruth "(a || b) && ! c"
+-- ["a", "b", "c"]
+-- ([False, False, False], False)
+-- ([False, False, True], False)
+-- ([False, True, False], True)
+-- ([False, True, True], False)
+-- ([True, False, False], True)
+-- ([True, False, True], False)
+-- ([True, True, False], True)
+-- ([True, True, True], False)
+-- >>> makeTruth "True"
+-- []
+-- ([], True)
 makeTruth :: String -> IO ()
-makeTruth s = case readExpr s of
+makeTruth s = case parseExpr s of
   Left err -> putStrLn $ errorBundlePretty err
   Right e -> printResult vars $ truth vars e
     where
-      expr = readExpr s
       vars = getVars e
