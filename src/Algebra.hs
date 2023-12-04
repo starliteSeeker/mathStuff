e599e5fb0f617dccb143e2f1c8c50e960522c365
Albert Lee <albert.lee.taiwan@gmail.com>
2023-12-04 08:47:17 +0800

    somewhat usable version of countdown

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -4,6 +4,15 @@ import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Writer
+  ( Writer,
+    runWriter,
+    tell,
+    writer,
+  )
+import Data.Bifunctor (second)
+import Data.List
+import Data.Ratio
+import qualified Data.Set as S
 
 -- | Differential list to use with Writer
 newtype DiffList a = DiffList {getList :: [a] -> [a]}
@@ -68,3 +77,87 @@ inverse :: Int -> Int -> MaybeT (Writer (DiffList String)) Int
 inverse a m = do
   (gcd, x, _) <- lift $ exEuclid a m
   if gcd == 1 then return $ mod x m else mzero
+
+type Val = Writer Expr Rational
+
+data Expr
+  = AddSub (S.Set Expr) (S.Set Expr)
+  | MulDiv (S.Set Expr) (S.Set Expr)
+  | R Integer
+  | Empty
+  deriving (Eq, Ord)
+
+instance Show Expr where
+  show Empty = "EMPTY"
+  show (R r) = show r
+  show (AddSub a b)
+    | S.size a == 1 && S.null b = show a
+    | otherwise = "(" ++ a' ++ if S.null b then [] else b' ++ ")"
+    where
+      a' = intercalate "+" $ map show $ S.elems a
+      b' = '-' : intercalate "-" (map show $ S.elems b)
+  show (MulDiv a b)
+    | S.size a == 1 && S.null b = show a
+    | otherwise = "(" ++ a' ++ if S.null b then [] else b' ++ ")"
+    where
+      a' = intercalate "*" $ map show $ S.elems a
+      b' = '-' : intercalate "/" (map show $ S.elems b)
+
+instance Semigroup Expr where
+  Empty <> x = x
+  x <> Empty = x
+  AddSub a b <> AddSub c d = AddSub (S.union a c) (S.union b d)
+  MulDiv a b <> MulDiv c d = MulDiv (S.union a c) (S.union b d)
+  x <> MulDiv c d = MulDiv (S.insert x c) d
+  x <> AddSub c d = AddSub (S.insert x c) d
+  _ <> R _ = undefined
+
+instance Monoid Expr where
+  mempty = Empty
+
+-- | Make the target number using all given numbers
+countdown :: [Integer] -> Integer -> [String]
+countdown given target =
+  let seed = map (\v -> tell (R v) >> return (v % 1)) given
+   in map show $ filter (\a -> fst (head a) == fromIntegral target) $ map (map runWriter) $ iterate (>>= (concatMap tt . takePairs)) [seed] !! (length given - 1)
+  where
+    takeOne :: [Val] -> [(Val, [Val])]
+    takeOne (l : ls) = (l, ls) : map (second (l :)) (takeOne ls)
+    takeOne [] = []
+    takePairs :: [Val] -> [(Val, Val, [Val])]
+    takePairs (l : ls) = map (\(a, s) -> (l, a, s)) (takeOne ls) ++ map (\(a, b, s) -> (a, b, l : s)) (takePairs ls)
+    takePairs [] = []
+    tt :: (Val, Val, [Val]) -> [[Val]]
+    tt (a, b, ls) =
+      [ a `addW` b : ls,
+        a `subW` b : ls,
+        b `subW` a : ls,
+        a `mulW` b : ls
+      ]
+        ++ [a `divW` b : ls | (fst . runWriter) b /= 0]
+        ++ [b `divW` a : ls | (fst . runWriter) a /= 0]
+    binOp :: Char -> (Rational -> Rational -> Rational) -> (Expr -> Expr) -> Val -> Val -> Val
+    binOp c op f a b =
+      let (aa, aw) = runWriter a
+          (ba, bw) = runWriter b
+       in writer (aa `op` ba, aw <> f bw)
+    addW :: Val -> Val -> Val
+    addW = binOp '+' (+) f
+      where
+        f x@(AddSub _ _) = x
+        f x = AddSub (S.singleton x) S.empty
+    subW :: Val -> Val -> Val
+    subW = binOp '-' (-) f
+      where
+        f (AddSub a b) = AddSub b a
+        f x = AddSub S.empty (S.singleton x)
+    mulW :: Val -> Val -> Val
+    mulW = binOp '*' (*) f
+      where
+        f x@(MulDiv _ _) = x
+        f x = MulDiv (S.singleton x) S.empty
+    divW :: Val -> Val -> Val
+    divW = binOp '/' (/) f
+      where
+        f (MulDiv a b) = MulDiv b a
+        f x = MulDiv S.empty (S.singleton x)
